6daab22ce38d58ba3ac2c9327a73de5ae75e373f
Har-sh-arma <harinathsharma303@gmail.com>
2022-10-20 13:04:32 +0530

    Added admin signup

--- models/Student_User.js
+++ models/Student_User.js
@@ -1,1 +1,11 @@
 //Script containing Schema for student User
+
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({name: String,
+					email: String,
+					pass_hash: String});
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var adminSignup = require('../actions/signup');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.post('/adminSignup', adminSignup);
 
 module.exports = router;
