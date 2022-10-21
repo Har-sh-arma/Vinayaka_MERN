c10429964a3624f923835765467cdc2fd8891703
Har-sh-arma <harinathsharma303@gmail.com>
2022-10-21 09:36:23 +0530

    Login as well as signup done

--- actions/auth.js
+++ actions/auth.js
@@ -1,1 +1,40 @@
 //Script to ensure secure login and authentication for user persistence
+
+
+//First Login using bcrypt
+const mongoose = require('mongoose');
+const Student = require('../models/Student_User');
+const Admin = require('../models/Admin_User');
+const asyncWrapper = require('../function_mods/async');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const login = asyncWrapper( async( req, res, next) =>{
+	try{
+		if(await Student.findOne({ email: req.body.email})){
+			stu = await Student.findOne({ email: req.body.email});
+			if(await bcrypt.compare( req.body.password, stu.passHash)){
+				res.send("Authenticated Student");
+			}
+			else{res.send("Invalid Password");}
+		}
+		else if(await Admin.findOne({ email: req.body.email})){
+			ad = await Admin.findOne({ email: req.body.email});
+			if(await bcrypt.compare( req.body.password, ad.passHash)){
+				res.send("Authenticated Admin");
+			}	
+			else{res.send("Invalid Password");}
+		}
+		else{
+			res.send("Invalid Credentials");
+		}
+        }catch(err){
+                console.log(err);
+        }
+});
+
+
+//authentication for hydration jwt
+
+
+module.exports = {login}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var {adminSignup, studentSignup} = require('../actions/signup');
+var {login} = require('../actions/auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,7 +10,8 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/adminSignup', adminSignup);
-
 router.post('/studentSignup', studentSignup);
+router.post('/login', login);
+
 
 module.exports = router;
